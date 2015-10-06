0167ec3ac1679dedcb801825844655d8e7dde96f
Luke Kung <lckung@gmail.com>
2015-10-06 11:15:12 -0700

    Update configs for YARN to run mapreduce.

--- deploy_templates.py
+++ deploy_templates.py
@@ -76,6 +76,7 @@ template_vars = {
   "java_home": os.getenv("JAVA_HOME"),
   "default_tachyon_mem": "%dMB" % tachyon_mb,
   "system_ram_mb": "%d" % system_ram_mb,
+  "yarn_nodemanager_mem_mb": "%d" % slave_ram_mb,
   "aws_access_key_id": os.getenv("AWS_ACCESS_KEY_ID"),
   "aws_secret_access_key": os.getenv("AWS_SECRET_ACCESS_KEY"),
 }
