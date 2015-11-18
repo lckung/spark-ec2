19a08a393b91532ffb4b4ebe77de60e2e81b879a
Luke Kung <lckung@gmail.com>
2015-11-18 22:20:55 +0000

    Give more memory to Yarn on each node.

--- deploy_templates.py
+++ deploy_templates.py
@@ -34,11 +34,11 @@ system_ram_kb = min(slave_ram_kb, master_ram_kb)
 system_ram_mb = system_ram_kb / 1024
 slave_ram_mb = slave_ram_kb / 1024
 # Leave some RAM for the OS, Hadoop daemons, and system caches
-if slave_ram_mb > 100*1024:
-  slave_ram_mb = slave_ram_mb - 15 * 1024 # Leave 15 GB RAM
-elif slave_ram_mb > 60*1024:
-  slave_ram_mb = slave_ram_mb - 10 * 1024 # Leave 10 GB RAM
-elif slave_ram_mb > 40*1024:
+#if slave_ram_mb > 100*1024:
+#  slave_ram_mb = slave_ram_mb - 15 * 1024 # Leave 15 GB RAM
+#elif slave_ram_mb > 60*1024:
+#  slave_ram_mb = slave_ram_mb - 10 * 1024 # Leave 10 GB RAM
+if slave_ram_mb > 40*1024:
   slave_ram_mb = slave_ram_mb - 6 * 1024 # Leave 6 GB RAM
 elif slave_ram_mb > 20*1024:
   slave_ram_mb = slave_ram_mb - 3 * 1024 # Leave 3 GB RAM
